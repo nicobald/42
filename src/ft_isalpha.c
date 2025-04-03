8940e49d5a8458cedccc0fbe7c16448118016228
nbaldes <nbaldes@student.42.fr>
2025-04-03 13:47:36 +0200

    update

--- src/ft_isalpha.c
+++ src/ft_isalpha.c
@@ -6,7 +6,16 @@
 /*   By: utilisateur <utilisateur@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 10:35:26 by utilisateur       #+#    #+#             */
-/*   Updated: 2025/03/27 10:35:27 by utilisateur      ###   ########.fr       */
+/*   Updated: 2025/04/03 13:46:20 by utilisateur      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int ft_isalpha(char c) // check if char c is alphabetic
+{
+    if ((c >= 'a' && c <= 'z')
+        || (c >= 'A' && c <= 'Z'))
+        return (1);
+    return (0);
+}
