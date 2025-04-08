07b08b84b7f6d57a6d089b939d5494429822d2bc
nbaldes <nbaldes@student.42.fr>
2025-04-08 10:07:18 +0200

    itoa striteri

--- src/ft_itoa.c
+++ src/ft_itoa.c
@@ -3,10 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nbaldes <nbaldes@student.42.fr>            +#+  +:+       +#+        */
+/*   By: utilisateur <utilisateur@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 16:44:26 by nbaldes           #+#    #+#             */
-/*   Updated: 2025/04/03 16:47:55 by nbaldes          ###   ########.fr       */
+/*   Updated: 2025/04/08 09:53:18 by utilisateur      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void recurs(int n, char *str, int *i)
+{
+    if (n / 10 != 0)
+        recurs(n / 10, str, i)
+    str[*i] = n (n % 10) + '0';
+    *i++;
+    return ;
+}
+
+char *itoa(int n)
+{
+    int i;
+
+    i = 0;
+    char *str = malloc(12);
+    if (n < 0)
+    {
+        str[i] = '-';
+        i++;
+        n = -n;
+    }
+    recurs(n, str, i)
+    str[i] = '\0';
+    return (str);
+}
--- src/ft_striteri.c
+++ src/ft_striteri.c
@@ -3,10 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nbaldes <nbaldes@student.42.fr>            +#+  +:+       +#+        */
+/*   By: utilisateur <utilisateur@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 16:45:12 by nbaldes           #+#    #+#             */
-/*   Updated: 2025/04/03 16:46:35 by nbaldes          ###   ########.fr       */
+/*   Updated: 2025/04/08 10:05:54 by utilisateur      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    unsigned int i;
+
+    i = 0;
+    result = malloc(ft_strlen(s) + 1)
+    if (!s)
+        return (NULL);
+    while(s[i])
+    {
+        (*f)(i, s);
+    }
+}
